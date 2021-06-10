a2f51a11fd4fe6f377d369d28c876e73ac256292
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-06-10 19:12:25 -0300

    Solução da tarefa 03

--- Aula20210527/scripts.js
+++ Aula20210527/scripts.js
@@ -1,3 +1,10 @@
-function funcOlaImagem(){
-   alert("Imagem de logo!");
+function funcOlaImagem(img){
+   switch(img){
+      case 1:
+         alert("Imagem de logo!");
+         break;
+      case 2:
+         alert("Imagem de CSS!");
+         break;
+   }   
 }
