ed13d82f4e0929be02a54f1b2e304c3873c5dbff
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-07-01 20:44:42 -0300

    Aula do dia 01/07/2021 - PHP rodando em servidor. Criado conexão com banco de dados; seleção de dados no banco de dados e exibindo em tabela.

---
+++ Aula20210701/bd/alunos.sql
@@ -0,0 +1,59 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.0.16-nt
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema alunos
+--
+
+CREATE DATABASE IF NOT EXISTS alunos;
+USE alunos;
+
+--
+-- Definition of table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `idusuarios` int(10) unsigned NOT NULL auto_increment,
+  `nome` varchar(100) NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `dica` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY  (`idusuarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`idusuarios`,`nome`,`login`,`senha`,`dica`,`email`) VALUES 
+ (1,'Eduardo Henrique','eduardo','123456','123456','eduardo.marques@uemg.br'),
+ (2,'A Fulano 1','fulano1','123456','123456','a@a.com'),
+ (3,'Fulano 2','fulano2','123456','123456','b@b.com');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
