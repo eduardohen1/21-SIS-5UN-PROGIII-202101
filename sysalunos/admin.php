0135bd03e528f6c478a7cbd43245591748d6e722
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-07-08 20:46:10 -0300

    Aula de PHP dia 08/07/2021. Criando tela de login, consulta em BD e validação e redirecionamento para tela de admin.

---
+++ sysalunos/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de gerenciamento de alunos</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js" ></script>
+    <link rel="shortcut icon" href="img/studentmeets_4873.ico" />
+</head>
+<body>
+    <?php
+        $nome = $_GET["nome"];
+        echo "<hr><h1>Bem vindo $nome!!!!</h1><hr>";
+
+    ?>
+</body>
+</html>
